e4eb5bc3229d44008efa5ed8cceb99af6835107f
wyizhen <wang.yizhen@intellif.com>
2021-11-22 18:46:07 +0800

    init

---
+++ src/views/TopHeader.vue
@@ -0,0 +1,53 @@
+<template>
+  <header class="top-header">
+    <div class="logo"></div>
+    <div class="center">
+      <a-icon
+        :type="menuCollapsed ? 'menu-unfold' : 'menu-fold'"
+        @click="toggleCollapse"
+      />
+      <span>欢迎！</span>
+    </div>
+    <div class="right"></div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    menuCollapsed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    toggleCollapse() {
+      this.$emit('update:menuCollapsed', !this.menuCollapsed)
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.top-header {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  .logo {
+    width: 210px;
+  }
+  .logo-expand {
+    width: 80px;
+  }
+  .center {
+    color: white;
+    i {
+      font-size: 22px;
+      margin-right: 10px;
+    }
+    span {
+      font-size: 14px;
+    }
+  }
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,17 +1,58 @@
 <template>
-  <a-layout id="layout">
-    <a-layout-header>Header</a-layout-header>
-    <a-layout>
-      <a-layout-sider>Sider</a-layout-sider>
-      <a-layout-content>
-        <router-view></router-view>
-      </a-layout-content>
-    </a-layout>
-  </a-layout>
+  <div id="layout">
+    <top-header class="header" :menuCollapsed.sync="menuCollapsed"></top-header>
+    <aside :class="menuCollapsed ? 'asider-expand' : 'asider'">
+      <left-menu :menuCollapsed="menuCollapsed"></left-menu>
+    </aside>
+    <main class="content">
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
 <script>
-export default {}
+import LeftMenu from './LeftMenu'
+import TopHeader from './TopHeader'
+export default {
+  name: 'Index',
+  components: {
+    TopHeader,
+    LeftMenu,
+  },
+  data() {
+    return {
+      menuCollapsed: false,
+    }
+  },
+}
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+#layout {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  .header {
+    background-color: @primary-color;
+    width: 100%;
+    height: 60px;
+    padding: 0;
+  }
+  .asider {
+    width: 210px;
+    height: calc(100% - 60px);
+    background-color: white;
+  }
+  .asider-expand {
+    width: 80px;
+    height: calc(100% - 60px);
+    background-color: white;
+  }
+  .content {
+    flex: 1;
+    height: calc(100% - 60px);
+    background: #f0f2f5;
+  }
+}
+</style>

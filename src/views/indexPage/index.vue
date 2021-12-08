1ec677aaf6f64cae0fc2d573c1781c68874c7963
wyizhen <wang.yizhen@intellif.com>
2021-12-08 18:37:09 +0800

    init

--- src/views/indexPage/index.vue
+++ src/views/indexPage/index.vue
@@ -1,11 +1,65 @@
 <template>
-  <div>首页</div>
+  <div>
+    <div class="plan" v-for="(plan, index) in planList" :key="index">
+      <div class="plan-title">{{ plan.title }}</div>
+      <div class="plan-list">
+        <div
+          class="plan-item"
+          v-for="(item, index) in planSubList"
+          :key="index"
+        >
+          {{ item.title }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      planList: [
+        { title: '当天整体计划', key: 'overAll' },
+        { title: '当天物流计划', key: 'logistics' },
+        { title: '当天外物流计划', key: 'overLogistics' }
+      ],
+      planSubList: [
+        { title: '计划总数', key: 'total' },
+        { title: '未调度', key: 'total1' },
+        { title: '已调度', key: 'total2' },
+        { title: '充装中', key: 'total3' },
+        { title: '运输中', key: 'total4' },
+        { title: '已到达', key: 'total5' },
+        { title: '已卸货', key: 'total6' }
+      ]
+    }
+  }
 }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.plan {
+  height: 150px;
+  width: 100%;
+  .plan-title {
+    height: 30px;
+    line-height: 30px;
+    font-size: 14px;
+    padding: 0px 10px;
+    background: #cbcbcb;
+  }
+  .plan-list {
+    display: flex;
+    font-size: 14px;
+    .plan-item {
+      height: 100px;
+      flex: 1;
+      margin: 10px;
+      border-radius: 5px;
+      background: aquamarine;
+    }
+  }
+}
+</style>

c826c9cf3835b39ff7f55d0aa5c44f9c8fd35225
wyizhen <wyizheny@163.com>
2021-12-24 11:00:33 +0800

    测试组合搜索框的使用

--- src/views/basicInfo/carRireManage/index.vue
+++ src/views/basicInfo/carRireManage/index.vue
@@ -1,13 +1,44 @@
 <!-- 车辆轮胎管理 -->
 <template>
-  <div class="container">车辆轮胎管理</div>
+  <div class="container">
+    车辆轮胎管理
+    <a-combo-grid
+      v-model="value"
+      labelField="name"
+      :columns="columns"
+      :tableData="tableData"
+      style="width: 300px"
+      tableWidth="600px"
+    ></a-combo-grid>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {}
-  }
+    return {
+      value: '',
+      columns: [
+        { title: '证件号', dataIndex: 'id' },
+        { title: '姓名', dataIndex: 'name' },
+        { title: '年龄', dataIndex: 'age' },
+        { title: '地址', dataIndex: 'address' }
+      ],
+      tableData: [
+        { id: 1, name: '路飞', age: '16' },
+        { id: 2, name: '索隆', age: '16' },
+        { id: 3, name: '娜美', age: '14' },
+        { id: 4, name: '乌索普', age: '14' },
+        { id: 5, name: '山治', age: '14' },
+        { id: 6, name: '乔巴', age: '14' },
+        { id: 7, name: '罗宾', age: '14' },
+        { id: 8, name: '弗兰奇', age: '14' },
+        { id: 9, name: '布鲁克', age: '14' },
+        { id: 10, name: '甚平', age: '14' }
+      ]
+    }
+  },
+  components: {}
 }
 </script>
 
